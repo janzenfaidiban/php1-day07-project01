289c0e25b6379f1a1ab384e9290aeae3f4255c67
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-06-13 18:43:43 +0900

    first push

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- CONTENT AREA -->
+<div class="col-9">
+    <!-- .card -->
+    <div class="card">
+        <div class="card-header">
+            <h1>Dasbor</h1>
+        </div>
+        <div class="card-body">
+            <p>Welcome!</p>
+        </div>
+
+        <div class="card-footer">
+
+        </div>
+    </div>
+    <!-- end .card -->
+</div>
+<!-- END CONTENT AREA -->
